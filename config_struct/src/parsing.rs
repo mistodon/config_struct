84d594870e6c776d1f9350ef104579abb62ef752
Pirh <pirh.badger@gmail.com>
2017-12-19 22:03:51 +0000

    Reduce repitition in parsing modules

---
+++ config_struct/src/parsing.rs
@@ -0,0 +1,38 @@
+use std::collections::BTreeMap;
+use std::io;
+use std::path::Path;
+
+use value::{ RawValue, RawStructValue };
+
+
+pub type ParsedConfig<T> = BTreeMap<String, T>;
+
+
+pub fn parsed_to_raw_config<T, F>(
+    parsed_config: ParsedConfig<T>,
+    convert_fn: F) -> RawStructValue
+where
+    F: Fn(&str, &str, T) -> RawValue
+{
+    let struct_name = "Config".to_owned();
+
+    let fields = parsed_config.into_iter().map(|(key, value)|
+    {
+        let value = convert_fn("_Config", &key, value);
+        (key, value)
+    }).collect();
+
+    RawStructValue { struct_name, fields }
+}
+
+pub fn slurp_file(path: &Path) -> Result<String, io::Error>
+{
+    use std::fs::File;
+    use std::io::Read;
+
+    let mut buffer = String::new();
+    let file = &mut File::open(&path)?;
+    file.read_to_string(&mut buffer)?;
+
+    Ok(buffer)
+}
--- config_struct/src/toml_parsing.rs
+++ config_struct/src/toml_parsing.rs
@@ -8,34 +8,20 @@ use { RawValue, RawStructValue };
 
 pub fn parse_config<S: AsRef<str>>(config_source: S) -> Result<RawStructValue, Error>
 {
-    use std::collections::BTreeMap;
+    use parsing::{ self, ParsedConfig };
 
-    let toml_object: BTreeMap<String, Value> = toml::from_str(config_source.as_ref())?;
+    let toml_object: ParsedConfig<Value> = toml::from_str(config_source.as_ref())?;
 
-    let raw_config: RawStructValue = {
-        let struct_name = "Config".to_owned();
-        let fields = toml_object.into_iter().map(|(key, value)|
-        {
-            let value = toml_to_raw_value("_Config", &key, value);
-            (key, value)
-        }).collect();
-        RawStructValue { struct_name, fields }
-    };
+    let raw_config = parsing::parsed_to_raw_config(toml_object, toml_to_raw_value);
 
     Ok(raw_config)
 }
 
 pub fn parse_config_from_file<P: AsRef<Path>>(config_path: P) -> Result<RawStructValue, Error>
 {
-    let config_source = {
-        use std::fs::File;
-        use std::io::Read;
+    use parsing;
 
-        let mut buffer = String::new();
-        let file = &mut File::open(&config_path)?;
-        file.read_to_string(&mut buffer)?;
-        buffer
-    };
+    let config_source = parsing::slurp_file(config_path.as_ref())?;
 
     parse_config(&config_source)
 }
