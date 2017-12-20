53adb6d42582c561b13f3793bc85f3c71764df14
Vi <1149443+mistodon@users.noreply.github.com>
2017-12-20 03:15:37 +0000

    Support JSON config files

--- config_struct_test/src/lib.rs
+++ config_struct_test/src/lib.rs
@@ -3,11 +3,69 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate ron;
+extern crate serde_json;
 extern crate serde_yaml;
 extern crate toml;
 
 
 mod config;
+
+
+mod json_tests
+{
+    use std;
+    use serde_json;
+
+    use config::json::{ Config, CONFIG };
+
+    #[test]
+    fn test_declarations()
+    {
+        let _conf: &Config = &CONFIG;
+    }
+
+    #[test]
+    fn test_deserialization()
+    {
+        let json_source = include_str!("../config.json");
+        let conf: Config = serde_json::from_str(json_source).unwrap();
+        assert_eq!(conf.name, "Config name");
+    }
+
+    #[test]
+    fn test_simple_values()
+    {
+        assert_eq!(CONFIG.name, "Config name");
+        assert_eq!(CONFIG.nothing, None);
+        assert_eq!(CONFIG.number, 100);
+        assert_eq!(CONFIG.is_config, true);
+        assert_eq!(CONFIG.is_not_config, false);
+        assert_eq!(CONFIG.i64_max, std::i64::MAX);
+        assert_eq!(CONFIG.u64_max, std::u64::MAX);
+        assert_eq!(CONFIG.floaty, 123.456789);
+    }
+
+    #[test]
+    fn test_composite_values()
+    {
+        assert_eq!(CONFIG.coord, [-5.0, 5.0].as_ref());
+        assert_eq!(CONFIG.nested.name, "nested2");
+        assert_eq!(CONFIG.nested.values.x, 0);
+        assert_eq!(CONFIG.nested.values.y, 1);
+        assert_eq!(CONFIG.nested.values.z, 2);
+        assert_eq!(CONFIG.array_of_structs[0].name, "first");
+        assert_eq!(CONFIG.array_of_structs[1].name, "second");
+        assert_eq!(CONFIG.array_of_structs[0].n, 0);
+        assert_eq!(CONFIG.array_of_structs[1].n, 1);
+    }
+
+    #[test]
+    fn test_empty_array_is_array_of_unit()
+    {
+        let empty: &[()] = &[];
+        assert_eq!(CONFIG.empty, empty);
+    }
+}
 
 
 mod ron_tests
