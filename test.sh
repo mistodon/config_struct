d62ae73261dc5deefeafaf584e325c91f70dc121
Claire Harris <wishing.engine@gmail.com>
2018-07-31 17:18:04 +0100

    Add tests for dynamic loading

--- test.sh
+++ test.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+echo -e "\033[36;1mRunning library tests:\033[0m"
 (cd config_struct && cargo test --all-features)
+
+echo -e "\033[36;1mRunning integration tests:\033[0m"
 (cd config_struct_test && cargo test)
+
+echo -e "\033[36;1mRunning release-mode integration tests:\033[0m"
+(cd config_struct_test && cargo test --release)
